6389de3306f0152cac23f71e0e0d0fad79040a32
Saket Amraotkar <saket.amraotkar@tcs.com>
2025-10-02 04:28:22 +0000

    add action in UI

---
+++ app/uiempcapm/webapp/ext/controller/StatusChange.js
@@ -0,0 +1,32 @@
+sap.ui.define([
+    "sap/m/MessageToast"
+], function(MessageToast) {
+    'use strict';
+
+    return {
+        /**
+         * Generated event handler.
+         *
+         * @param oContext the context of the page on which the event was fired. `undefined` for list report page.
+         * @param aSelectedContexts the selected contexts of the table rows.
+         */
+        onStatusChange: function(oContext, aSelectedContexts) {
+            // MessageToast.show("Custom handler invoked.");
+            var oID = oContext.getObject().ID;
+            var oAction = "updateEMPStatus";
+            var omInParameters = {
+                    model: this.getModel(),
+                    parameterValues: [{
+                        type: "employeeID",
+                        value: oID
+                    }],
+                    skipParameterDialog: true
+            };
+            this.editFlow.invokeAction(oAction,omInParameters).then(function(response){
+                if(response){
+
+                }
+            }.bind(this));
+        }
+    };
+});
